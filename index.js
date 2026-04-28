27321fb32f03267e24bd80e7ca46355b2f3f976e
manilawestern-debug <manilawestern@gmail.com>
2026-04-28 10:36:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+// homepage (optional)
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+// stream endpoint
 app.get("/stream", async (req, res) => {
   try {
-    const url = "https://streamfree.app/live/detroit-pistons-vs-orlando-magic1080p/index.m3u8?_t=HiOZiFb6COU2a4gX26pTjQ&_e=1777371201&_n=70c1b0084c0f6b02";
+    const url = "
+https://streamfree.app/live/detroit-pistons-vs-orlando-magic1080p/index.m3u8?_t=HiOZiFb6COU2a4gX26pTjQ&_e=1777371201&_n=70c1b0084c0f6b02";
 
     const response = await fetch(url, {
       headers: {
@@ -23,4 +35,9 @@ app.get("/stream", async (req, res) => {
   } catch (err) {
     res.send("ERROR");
   }
+});
+
+// start server
+app.listen(3000, () => {
+  console.log("Server running");
 });
